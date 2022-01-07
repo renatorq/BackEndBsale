f70e5d6ff672ef1cca0fd96096a364f9f57107de
renatorq <renato_rq_987@hotmail.com>
2022-01-07 18:18:34 -0500

    descarga de plantilla springboot

---
+++ src/main/java/com/bsale/bsale/BsaleApplication.java
@@ -0,0 +1,13 @@
+package com.bsale.bsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsale/bsale/BsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsale.bsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
